ec28dce99f4104b60964296832209e74d6b4f461
Sam Carter <sam.w.carter@gmail.com>
2016-11-17 09:24:05 +0000

    Update app.fsx

--- app.fsx
+++ app.fsx
@@ -48,16 +48,6 @@ let output_list input = List.map replacer input
 let folded_list list = list |> List.fold (+) ""
 let get_output_string = output_list >> folded_list
 
-let config = 
-    let port = System.Environment.GetEnvironmentVariable("PORT")
-    let ip127  = IPAddress.Parse("127.0.0.1")
-    let ipZero = IPAddress.Parse("0.0.0.0")
-
-    { defaultConfig with 
-        logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Verbose
-        bindings=[ (if port = null then HttpBinding.mk HTTP ip127 (uint16 8080)
-                    else HttpBinding.mk HTTP ipZero (uint16 port)) ] }
-
 let species = 
   [("Plant (tree)", "Baishan Fir"); ("Insect (butterfly)", "");
    ("Reptile", "Leaf scaled sea-snake");
